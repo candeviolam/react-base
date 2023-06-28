3986b0f8886ac06748d7a93d73d4529ab9e98e1d
unknown <candeviolam@gmail.com>
2023-06-28 17:13:06 -0300

    volví a poner el código de mi componente Noticia como estaba antes del useE y useS, ejemplo de html y css segmentado en el componente (Functional Component)

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,49 +1,37 @@
 // me prove del método para retornar el renderizado
-//              llamamos useEffect
-//                         elemento useState para procesar los arrays que me devuelve useEffect
-//                         useState para procesar los resultados que traje con el useEffect
-import React, { useEffect, useState } from "react";
+import React from "react";
 //       me importa las constantes automáticamente cuando la incrusté en la img del return
 import { CONSTANTS } from "../constants/image.constants";
+
+//como va a ser una función de flecha, se trata como variable, entonces para poder usarla la tengo que declarar antes afuera de Noticia
+const Parrafo = () => {
+  return <p>Noticia sobre el final de Pokemón</p>;
+};
 
 //puede ser una func tradicional o una flecha
 //       mi func se tiene que llamar igual que el nombre del componente
 function Noticia() {
-  //                                  useState para manejar el estado
-  const [characters, setCharacters] = useState([]);
+  const estado = false;
+  const estilos = { backgroundColor: "red", display: "flex" };
 
-  //el useEffect para traer los resultados (los personajes de Rick y Morty de la api) y dps procesamos esos resultados con el useState
-  useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      //           la primera respuesta se la tiene que pasar a json y esto me devuelve una promesa (ergo el segundo .then)
-      .then((res) => res.json())
-      //el segundo then me devuelve los resultados que tiene un objeto que se llama "results" adentro
-      //    lo tengo que desestructurar al obj de results (los paréntesis violetas)
-      .then(({ results }) => {
-        setCharacters(results);
-      })
-      //hago un catch que me va a hacer un error
-      .catch((err) => console.log(err));
-  }, []);
+  if (estado) {
+    estilos.backgroundColor = "blue";
+  } else {
+    estilos.backgroundColor = "green";
+  }
 
   return (
     // estas etiquetas "<> </>" se llaman fragment
+    //    el div recibe adentro style y style recibe adentro un objeto con backg... hecho ya variable, pero podría copiar todo el objeto adentro de style{} en lugar de hacer la variable
+    // Parrafo fue previamente declarado afuera arriba
     <>
-      {
-        //results es un array []
-        //        ? = operador de condición
-        characters?.map((character, index) => {
-          return (
-            <div key={index}>
-              <figure>
-                <img src={character.image} />
-                <figcaption>{character.name}</figcaption>
-              </figure>
-              <p>Noticia sobre el final de Pokemón</p>
-            </div>
-          );
-        })
-      }
+      <div style={estilos}>
+        <figure>
+          <img src={CONSTANTS.NOTICIA_PIKACHU} />
+          <figcaption>Pikachu</figcaption>
+        </figure>
+        <Parrafo />
+      </div>
     </>
   );
 }
