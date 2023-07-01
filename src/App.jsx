ec989eb3bd1a907cacfd6323e36ae00253aee321
unknown <candeviolam@gmail.com>
2023-07-01 17:56:56 -0300

    más ejemplos de useState para cuando escribo en un input, y objeto o array de objs como estado inicial de useState

--- src/App.jsx
+++ src/App.jsx
@@ -38,10 +38,33 @@ const msj = (counter) => (counter % 2 === 0 ? "Par" : "Impar");
 //aplicar el renderizado condicional (.red y .blue en App.css), vamos al span abajo
 function App() {
   const [counter, setCounter] = useState(0); // -> HOOK que se encarga de manejar el estado
-
   const [mensaje, setMensaje] = useState(msj(counter));
-
+  //ahora tengo una variable nombre que puedo usar p/mostrar en el html y modificarle el valor cuando suceda algún evento
+  const [nombre, setNombre] = useState("");
+  //                                   también puedo tener declarado un obj inicial
+  const [objeto, setObjeto] = useState({
+    ok: true,
+    hola: "Cómo estás?",
+  }); // -> voy a hacer que cuándo haga click en el botón +, se imprima en consola el obj ése (abajo)
+  /*
+  lo mismo sucedería si lo de arriba fuese un array (3 objs dentro del array)
+  const [objeto, setObjeto] = useState([
+    {
+    ok: true,
+    hola: "Cómo estás?",
+    },
+    {
+    ok: true,
+    hola: "Cómo estás?",
+    },
+    {
+    ok: true,
+    hola: "Cómo estás?",
+    },
+  ]);
+*/
   function handleAddClick() {
+    console.log(objeto);
     setCounter(counter + 1);
     handleMessageChange();
   }
@@ -55,12 +78,22 @@ function App() {
     setMensaje(msj(counter));
   }
 
+  //ésta func viene de abajo en el html en el input de nombre
+  //                         ésta si recibe "e"
+  function handleInputChange(e) {
+    //modificar el valor de nombre -> cuando se ejecute esa func (cuando el usuario escriba ("change") en el input), va a asignarle el valor al nombre
+    //ésta func se va a encargar de modificar el valor y (re)dibujar mi componente
+    setNombre(e.target.value);
+  }
+
   return (
+    //puedo tomar un fragmento como el Navbar, y teniendo el componente puedo llamarlo como una etiqueta html y asignarlo en cualquier lado
     //    renderizado condicional
     //   className porque estoy usando clases en App.css (.red, .blue)
     //                 cuando abro y cierro llaves (className) me permite agregar código de js
     //                  agrego la functión para definir si mi app tiene que pintarse de rojo o de azul
     <div className="counter-app">
+      <NavbarBoots />
       <span className={counter % 2 === 0 ? "blue" : "red"}>{mensaje}</span>
       <hr />
       <label>{counter}</label>
@@ -72,7 +105,14 @@ function App() {
           -
         </button>
       </div>
+      <hr />
+      <div>
+        <input type="text" name="nombre" onChange={handleInputChange} />
+        <label>{nombre}</label>
+      </div>
     </div>
+    //          éste "nombre" entre {} es el valor de la variable del useState de arriba -> "nombre", 1er elem del array que me devuelve el useState
+    //                                   equivalente al AddEventListener ("change", ) ... e.target.value ... de js
   );
 }
 
