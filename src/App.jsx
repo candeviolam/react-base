a27bac68296533b3df4d0e7e72c576dca415c374
unknown <candeviolam@gmail.com>
2023-07-05 18:49:03 -0300

    navegación sencilla desde el navbar, un navlink para ir a Pokemon y otro para ir a Contador con  la función setVista

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,9 @@ import { Pokemons } from "./components/pokemons/Pokemons";
 //acá dibujo toda mi aplicación, pero la puedo dividir y compartimentar en componentes
 //             pokemons lo vamos a recibir desde acá
 function App({ pokemons }) {
-  const [state, setState] = useState(false);
+  //     state es la variable que verifica cuál es que comp que se tiene que dibujar (!state abajo)
+  //            ésta func se encarga de modificar la variable que verifica el renderizado de la vista
+  const [state, setVista] = useState(true); // le pongo true para que me dibuje primero los pokemons (?
   const [counter, setCounter] = useState(0);
   return (
     //Higher Order Component -> un componente (hijo) dentro de otro componente, puedo pasarle varios hijos, y c/u de éstos hijos tiene su estado personal (CouterApp tiene su propio estado que es el del useState, y Pokemons tiene su propio estado que es el de los pokemons)
@@ -18,14 +20,17 @@ function App({ pokemons }) {
     // pasar componentes por props (lo que está adentro de <NavbarBoots>..</>) (un componente es html, así que acá podría recibir cualq cosa (por ej un div, etc))
     //                le paso tanto con como setCon -> tengo que ponerlos en CounterApp.jsx como props --> ésto es para que cuando yo haga el "Cambio Vista" con el botón, el estado de mi contador debería permanecer donde lo haya dejado -> ahora al estado no estoy manejándolo desde CounterApp(.jsx) que se refresca, sino que lo estoy manejando desde App
     //                                     setCounter es la función como prop (?
-    <NavbarBoots>
+    //           a NavbarB le vamos a pasar setVista (la entre {} puede tener otro nombre, pero tiene que ser el mismo que está en la constante de arriba)
+    //                     es la función que nos va a permitir modificar el estado
+    //  negando el state (!) porque tenía cruzados los botones en el navbar del navegador
+    //le estoy pasando como parám al Navbar la función setVista -> que la recibe en NavbarBoots.jsx desestructurando desde props
+    <NavbarBoots setVista={setVista}> 
       <div>
-        {state ? (
+        {!state ? (
           <CounterApp con={counter} setCon={setCounter} />
         ) : (
           <Pokemons pokemones={pokemons} />
         )}
-        <button onClick={() => setState(!state)}>Cambiar Vista</button>
       </div>
     </NavbarBoots>
   );
--- src/components/NavbarBoots.jsx
+++ src/components/NavbarBoots.jsx
@@ -9,8 +9,12 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 //                   vamos a recibir por prop el nuevo elemento (el div abajo del navbar) -> vamos a renderizar lo que me pase por prop
 //"children" (comps. dentro de otro comp.) -> nomenclatura que se utiliza p/enviar un prop
 function NavbarBoots(prop) {
+  //                a la func setVista la recibo como prop y la estoy enviando desde App.jsx
   //1ero desestructuro children de prop, y acto seguido lo renderizo abajo (en el div abajo de navbar)
-  const { children } = prop;
+  //                además del children vamos a recibir el setVista que nos va a permitir cambiar la vista (tengo que pasar setVista por App.jsx tmb)
+  //                NavbarB recibe la func setVista, que la estamos desestructurando desde props, y por dentro estamos yendo hasta un navlink (por ej el de home), y cuando le hacemos click primero estamos evitando que se comporte como un navlink(es decir, que intente navegar hacia otro sitio web) y tmb estamos cambiando el setVista a true
+  //             el preventDelfault es porque como es un navlink, no queremos que se ejecute la navegación
+  const { children, setVista } = prop;
   return (
     <div>
       <Navbar bg="light" expand="lg">
@@ -19,8 +23,24 @@ function NavbarBoots(prop) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link
+                href="#home"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setVista(true);
+                }}
+              >
+                Pokemon
+              </Nav.Link>
+              <Nav.Link
+                href="#link"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setVista(false);
+                }}
+              >
+                Contador
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
